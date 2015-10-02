9fff4898497c886fdd7d29165eacd4b8ce6ffb47
blu <blu@ridi.com>
2015-10-02 14:35:37 +0900

    조직도
     - TA분들은 못보게

--- src/controls/organization/chart.php
+++ src/controls/organization/chart.php
@@ -1,10 +1,16 @@
 <?php
 use Intra\Model\LightFileModel;
 use Intra\Service\Ridi;
+use Intra\Service\UserSession;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 if (!Ridi::isRidiIP()) {
+	throw new Exception('권한이 없습니다.');
+}
+
+$self = UserSession::getSelf();
+if (preg_match('/TA/', $self->getName())) {
 	throw new Exception('권한이 없습니다.');
 }
 
