6451950ebc880d76d4e0af03f0c23c2f431cedcc
blu <blu@ridi.com>
2016-01-04 15:39:08 +0900

    재선님 퇴사로 한진규님께 인수인계

--- src/Intra/Service/UserPayment.php
+++ src/Intra/Service/UserPayment.php
@@ -262,7 +262,6 @@ class UserPayment
 		$receivers[] = '***REMOVED***';
 		$receivers[] = '***REMOVED***';
 		$receivers[] = '***REMOVED***';
-		$receivers[] = '***REMOVED***';
 
 		$mg = new Mailgun("***REMOVED***");
 		$domain = "ridibooks.com";
