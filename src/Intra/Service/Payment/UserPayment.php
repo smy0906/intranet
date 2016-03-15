a471c9c3bbfc2a5fe23a100ed078b30be061309b
blu <blu@ridi.com>
2016-03-15 16:26:29 +0900

    권한 변경
     - 민규님 진규님 제외

--- src/Intra/Service/Payment/UserPayment.php
+++ src/Intra/Service/Payment/UserPayment.php
@@ -263,7 +263,6 @@ class UserPayment
 	{
 		$receivers[] = '***REMOVED***';
 		$receivers[] = '***REMOVED***';
-		$receivers[] = '***REMOVED***';
 
 		$mg = new Mailgun("***REMOVED***");
 		$domain = "ridibooks.com";
