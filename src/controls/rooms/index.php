d7c0be89dfe349af6cae5f2e8b733c7922440886
blu <blu@ridi.com>
2016-03-27 17:08:13 +0900

    회의실
     - is_visible 추가

--- src/controls/rooms/index.php
+++ src/controls/rooms/index.php
@@ -4,7 +4,7 @@
 
 $db = \Intra\Service\IntraDb::getGnfDb();
 
-$rooms = $db->sqlDicts('select * from rooms');
+$rooms = $db->sqlDicts('select * from rooms  where is_visible = "1"');
 $name = \Intra\Service\User\UserSession::getSelfDto()->name;
 
 return compact('rooms', 'name');
