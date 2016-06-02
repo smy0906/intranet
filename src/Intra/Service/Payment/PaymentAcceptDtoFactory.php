1cf102d5f7b10482b7f9dbfff57ea3b69704b950
blu <blu@ridi.com>
2016-06-02 23:02:41 +0900

    PaymentAcceptDtoFactory aviod warning

--- src/Intra/Service/Payment/PaymentAcceptDtoFactory.php
+++ src/Intra/Service/Payment/PaymentAcceptDtoFactory.php
@@ -15,8 +15,10 @@ class PaymentAcceptDtoFactory
 	public static function createFromDatabaseDicts($payment_accepts_dicts)
 	{
 		$return = [];
-		foreach ($payment_accepts_dicts as $payment_accepts_dict) {
-			$return[] = PaymentAcceptDto::importFromDatabaseDict($payment_accepts_dict);
+		if (is_array($payment_accepts_dicts)) {
+			foreach ($payment_accepts_dicts as $payment_accepts_dict) {
+				$return[] = PaymentAcceptDto::importFromDatabaseDict($payment_accepts_dict);
+			}
 		}
 		return $return;
 	}
