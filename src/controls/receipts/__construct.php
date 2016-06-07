ea90bbdc9c1b2fd6417045896177630e83b29f01
blu <blu@ridi.com>
2016-06-07 18:50:11 +0900

    영수증 처리 다영님 권한 추가 #2

---
+++ src/controls/receipts/__construct.php
@@ -0,0 +1,12 @@
+<?php
+/** @var $this Intra\Core\Control */
+use Intra\Service\User\UserPolicy;
+use Intra\Service\User\UserSession;
+
+$is_receipts_admin = UserPolicy::isReceiptsAdmin(UserSession::getSelfDto());
+$response = $this->getResponse();
+$response->add(
+	[
+		'isAdmin' => $is_receipts_admin,
+	]
+);
