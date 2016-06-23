30c5f05855135ce6448873bd0f402ae6321779ce
blu <blu@ridi.com>
2016-06-23 12:13:20 +0900

    결제요청 메일 테스트 중

--- src/Intra/Service/Cron/PaymentCron.php
+++ src/Intra/Service/Cron/PaymentCron.php
@@ -2,6 +2,7 @@
 
 namespace Intra\Service\Cron;
 
+use Intra\Lib\ObjectsUtils;
 use Intra\Model\PaymentModel;
 use Intra\Service\Payment\PaymentDtoFactory;
 use Intra\Service\User\UserService;
@@ -38,28 +39,33 @@ class PaymentCron extends CronInterface
 		$return_dtos = [];
 		$dicts = PaymentModel::getRemindMailBefore3days();
 		$payments = PaymentDtoFactory::importFromDatabaseDicts($dicts);
-		foreach ($payments as $payment) {
+		$payments_by_uid = ObjectsUtils::alignListByKey($payments, 'uid');
+
+		foreach ($payments_by_uid as $uid => $payments) {
+			$first_payment = $payments[0];
 			$dto = clone $dto_template;
 			$dto->receiver = [
-				UserService::getEmailByUidSafe($payment->manager_uid),
-				UserService::getEmailByUidSafe($payment->uid),
+				UserService::getEmailByUidSafe($first_payment->manager_uid),
+				UserService::getEmailByUidSafe($first_payment->uid),
 				'***REMOVED***',
 			];
-			$dto->dicts = [
-				[
-					'요청일' => $payment->request_date,
-					'요청자' => $payment->register_name,
-					'결제 예정일' => $payment->pay_date,
-					'귀속부서' => $payment->team,
-					'프로덕트' => $payment->product,
-					'분류' => $payment->category,
-					'상세내역' => $payment->desc,
-					'비고' => $payment->note,
-					'금액' => number_format($payment->price),
-					'결제수단' => $payment->paytype,
-					'상태' => $payment->status,
-				]
-			];
+			$dto->dicts = [];
+			foreach ($payments as $payment) {
+				$dto->dicts[] =
+					[
+						'요청일' => $payment->request_date,
+						'요청자' => $payment->register_name,
+						'결제 예정일' => $payment->pay_date,
+						'귀속부서' => $payment->team,
+						'프로덕트' => $payment->product,
+						'분류' => $payment->category,
+						'상세내역' => $payment->desc,
+						'비고' => $payment->note,
+						'금액' => number_format($payment->price),
+						'결제수단' => $payment->paytype,
+						'상태' => $payment->status,
+					];
+			}
 			$return_dtos[] = $dto;
 		}
 
