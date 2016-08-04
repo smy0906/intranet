f4b4899f56e6da22f9d25adef3635e41a3eb26c9
blu <blu@ridi.com>
2016-08-04 16:35:35 +0900

    포커스룸 추가

--- src/controls/rooms/index.php
+++ src/controls/rooms/index.php
@@ -1,10 +1,24 @@
 <?php
 /** @var $this Intra\Core\Control */
 
+use Intra\Service\IntraDb;
+use Intra\Service\User\UserSession;
 
-$db = \Intra\Service\IntraDb::getGnfDb();
+$db = IntraDb::getGnfDb();
 
-$rooms = $db->sqlDicts('select * from rooms  where is_visible = "1"');
-$name = \Intra\Service\User\UserSession::getSelfDto()->name;
+$request = $this->getRequest();
+$type = $request->get('type');
+
+if (!strlen($type)) {
+	$type = 'default';
+}
+
+$where = [
+	'is_visible' => 1,
+	'type' => $type,
+];
+
+$rooms = $db->sqlDicts('select * from rooms  where ?', sqlWhere($where));
+$name = UserSession::getSelfDto()->name;
 
 return compact('rooms', 'name');
