57df81938a288dae983288dac0bb4e18c83035af
Kang Ki Tae <kt.kang@ridi.com>
2016-12-02 15:10:39 +0900

    edit_public.php 삭제 후 edit기능 UserEditService 한 군데에서 처리하도록 수정

--- controls/users/edit.php
+++ controls/users/edit.php
@@ -1,27 +1,11 @@
 <?php
 /** @var $this Intra\Core\Control */
 
-use Intra\Service\User\UserDtoFactory;
-use Intra\Service\User\UserDtoHandler;
-use Intra\Service\User\UserSession;
+use Intra\Service\User\UserEditService;
 
 $request = $this->getRequest();
 $uid = $request->get('uid');
 $key = $request->get('key');
 $value = $request->get('value');
 
-if (!UserSession::isUserManager()) {
-	return '권한이 없습니다';
-}
-
-$user_dto = UserDtoFactory::createByUid($uid);
-if ($user_dto === null) {
-	return '오류';
-}
-
-$user = new UserDtoHandler(UserDtoFactory::createByUid($uid));
-$user->updateByKey($key, $value);
-
-$user_dto = UserDtoFactory::createByUid($uid);
-
-return $user_dto->$key;
+return UserEditService::updateInfo($uid, $key, $value);
