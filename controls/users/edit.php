71afda441fe369c01f94d8277fb7d79f5f6cb7b2
Kang Ki Tae <kt.kang@ridi.com>
2016-12-02 16:17:43 +0900

    Service에서 Response를 리턴하지 않도록 변경.

--- controls/users/edit.php
+++ controls/users/edit.php
@@ -2,10 +2,16 @@
 /** @var $this Intra\Core\Control */
 
 use Intra\Service\User\UserEditService;
+use Symfony\Component\HttpFoundation\Response;
 
 $request = $this->getRequest();
 $uid = $request->get('uid');
 $key = $request->get('key');
 $value = $request->get('value');
 
-return UserEditService::updateInfo($uid, $key, $value);
+if (UserEditService::updateInfo($uid, $key, $value) !== null) {
+	return Response::create($value, 200);
+
+} else {
+	return Response::create("server error", 503);
+}
--- controls/users/image.php
+++ controls/users/image.php
@@ -2,8 +2,16 @@
 /** @var $this Intra\Core\Control */
 
 use Intra\Service\User\UserEditService;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 $request = $this->getRequest();
 $uid = $request->get('uid');
 
-return UserEditService::getImage($uid);
+$file =  UserEditService::getImage($uid);
+if ($file !== null) {
+	return BinaryFileResponse::create($file, 200);
+
+} else {
+	return Response::create('file not exist', 404);
+}
