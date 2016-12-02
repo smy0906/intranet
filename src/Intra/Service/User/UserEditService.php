59f52dcc740930babaa92e3a3ed73b1f0df8d38a
Kang Ki Tae <kt.kang@ridi.com>
2016-12-02 15:10:39 +0900

    edit_public.php 삭제 후 edit기능 UserEditService 한 군데에서 처리하도록 수정

--- src/Intra/Service/User/UserEditService.php
+++ src/Intra/Service/User/UserEditService.php
@@ -4,6 +4,7 @@ namespace Intra\Service\User;
 
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Intra\Model\UserModel;
 use Intra\Model\LightFileModel;
 
 /**
@@ -39,5 +40,23 @@ class UserEditService
 			return new BinaryFileResponse($dest, 200);
 		}
 		return new Response('file not exist', 404);
+	}
+
+	public static function updateInfo($uid, $key, $value)
+	{
+		$self = UserSession::getSelfDto();
+		if (!$self) {
+			return '권한이 없습니다';
+		}
+
+		if ($self->uid != $uid
+			&& !UserSession::isUserManager()) {
+			return '권한이 없습니다';
+		}
+
+		UserModel::update($uid, [$key => $value]);
+
+		$user_dto = UserDtoFactory::createByUid($uid);
+		return $user_dto->$key;
 	}
 }
