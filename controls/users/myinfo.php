d16da4e7708ef2a9e813231992bc03c3a0667144
Kang Ki Tae <kt.kang@ridi.com>
2016-12-07 18:21:53 +0900

    불필요한 주석 제거

--- controls/users/myinfo.php
+++ controls/users/myinfo.php
@@ -5,20 +5,3 @@ use Intra\Service\User\UserSession;
 $dto = UserSession::getSelfDto();
 
 return ['info' => $dto];
-
-//	'uid' => $dto->uid,
-//	'title' => $dto->name,
-//	'info' => [
-//		'body' => [
-//			//'db key' => ['name', 'value', 'isEditable']
-//			'id' => ['아이디', $dto->id, false],
-//			'name' => ['이름', $dto->name, false],
-//			'team' => ['팀', $dto->team, false],
-//			'birth' => ['생년월일', $dto->birth, true],
-//			'mobile' => ['전화번호', $dto->mobile, true],
-//			'email' => ['이메일', $dto->email, false],
-//		],
-//		'image' => $dto->image,
-//		'comment' => $dto->comment,
-//	]
-//];
