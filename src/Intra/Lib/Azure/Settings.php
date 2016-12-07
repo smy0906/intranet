8dbde20aa5e965956903469d54322f8f49c3e2ed
Kang Ki Tae <kt.kang@ridi.com>
2016-12-07 18:04:36 +0900

    중요 변수들 config파일로 이동

--- src/Intra/Lib/Azure/Settings.php
+++ src/Intra/Lib/Azure/Settings.php
@@ -6,35 +6,10 @@ use Intra\Config\Config;
 
 class Settings
 {
-	private static $clientId = [
-		'ridi.com' => '***REMOVED***',
-		'studiod.co.kr' => '***REMOVED***'
-	];
-	private static $password = [
-		'ridi.com' => '***REMOVED***',
-		'studiod.co.kr' => '***REMOVED***'
-	];
-	private static $redirectURI = [
-		'ridi.com' => 'http://intra.ridi.com/usersession/login.azure',
-		'studiod.co.kr' => 'http://intra.studiod.co.kr/usersession/login.azure'
-	];
-	private static $resourceURI = [
-		'ridi.com' => 'https://graph.windows.net',
-		'studiod.co.kr' => 'https://graph.windows.net'
-	];
-	private static $appTenantDomainName = [
-		'ridi.com' => 'ridicorp.com',
-		'studiod.co.kr' => 'studiod.co.kr'
-	];
-	private static $apiVersion = [
-		'ridi.com' => 'api-version=2013-11-08',
-		'studiod.co.kr' => 'api-version=2013-11-08'
-	];
-
 	public static function getClientId()
 	{
 		$domain = self::getDomain();
-		return self::$clientId[$domain];
+		return Config::$azure['clientId'][$domain];
 	}
 
 	/**
@@ -49,30 +24,30 @@ class Settings
 	public static function getPassword()
 	{
 		$domain = self::getDomain();
-		return self::$password[$domain];
+		return Config::$azure['password'][$domain];
 	}
 
 	public static function getRediectURI()
 	{
 		$domain = self::getDomain();
-		return self::$redirectURI[$domain];
+		return Config::$azure['redirectURI'][$domain];
 	}
 
 	public static function getResourceURI()
 	{
 		$domain = self::getDomain();
-		return self::$resourceURI[$domain];
+		return Config::$azure['resourceURI'][$domain];
 	}
 
 	public static function getAppTenantDomainName()
 	{
 		$domain = self::getDomain();
-		return self::$appTenantDomainName[$domain];
+		return Config::$azure['$appTenantDomainName'][$domain];
 	}
 
 	public static function getApiVersion()
 	{
 		$domain = self::getDomain();
-		return self::$apiVersion[$domain];
+		return Config::$azure['$apiVersion'][$domain];
 	}
 }
