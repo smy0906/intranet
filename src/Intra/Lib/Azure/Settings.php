a1cfb72b5b8f76862f7334e2cb7ceefa1304b097
Kang Ki Tae <kt.kang@ridi.com>
2016-12-08 10:47:51 +0900

    문제 발생으로 Azure 로그인 관련 예전으로 되돌림

--- src/Intra/Lib/Azure/Settings.php
+++ src/Intra/Lib/Azure/Settings.php
@@ -1,4 +1,56 @@
 <?php
+//
+//namespace Intra\Lib\Azure;
+//
+//use Intra\Config\Config;
+//
+//class Settings
+//{
+//	public static function getClientId()
+//	{
+//		$domain = self::getDomain();
+//		return Config::$azure['clientId'][$domain];
+//	}
+//
+//	/**
+//	 * @return string
+//	 */
+//	private static function getDomain()
+//	{
+//		$domain = Config::$domain;
+//		return $domain;
+//	}
+//
+//	public static function getPassword()
+//	{
+//		$domain = self::getDomain();
+//		return Config::$azure['password'][$domain];
+//	}
+//
+//	public static function getRediectURI()
+//	{
+//		$domain = self::getDomain();
+//		return Config::$azure['redirectURI'][$domain];
+//	}
+//
+//	public static function getResourceURI()
+//	{
+//		$domain = self::getDomain();
+//		return Config::$azure['resourceURI'][$domain];
+//	}
+//
+//	public static function getAppTenantDomainName()
+//	{
+//		$domain = self::getDomain();
+//		return Config::$azure['$appTenantDomainName'][$domain];
+//	}
+//
+//	public static function getApiVersion()
+//	{
+//		$domain = self::getDomain();
+//		return Config::$azure['$apiVersion'][$domain];
+//	}
+//}
 
 namespace Intra\Lib\Azure;
 
@@ -6,10 +58,35 @@ use Intra\Config\Config;
 
 class Settings
 {
+	private static $clientId = [
+		'ridi.com' => '***REMOVED***',
+		'studiod.co.kr' => '***REMOVED***'
+	];
+	private static $password = [
+		'ridi.com' => '***REMOVED***',
+		'studiod.co.kr' => '***REMOVED***'
+	];
+	private static $redirectURI = [
+		'ridi.com' => 'http://intra.ridi.com/usersession/login.azure',
+		'studiod.co.kr' => 'http://intra.studiod.co.kr/usersession/login.azure'
+	];
+	private static $resourceURI = [
+		'ridi.com' => 'https://graph.windows.net',
+		'studiod.co.kr' => 'https://graph.windows.net'
+	];
+	private static $appTenantDomainName = [
+		'ridi.com' => 'ridicorp.com',
+		'studiod.co.kr' => 'studiod.co.kr'
+	];
+	private static $apiVersion = [
+		'ridi.com' => 'api-version=2013-11-08',
+		'studiod.co.kr' => 'api-version=2013-11-08'
+	];
+
 	public static function getClientId()
 	{
 		$domain = self::getDomain();
-		return Config::$azure['clientId'][$domain];
+		return self::$clientId[$domain];
 	}
 
 	/**
@@ -24,30 +101,30 @@ class Settings
 	public static function getPassword()
 	{
 		$domain = self::getDomain();
-		return Config::$azure['password'][$domain];
+		return self::$password[$domain];
 	}
 
 	public static function getRediectURI()
 	{
 		$domain = self::getDomain();
-		return Config::$azure['redirectURI'][$domain];
+		return self::$redirectURI[$domain];
 	}
 
 	public static function getResourceURI()
 	{
 		$domain = self::getDomain();
-		return Config::$azure['resourceURI'][$domain];
+		return self::$resourceURI[$domain];
 	}
 
 	public static function getAppTenantDomainName()
 	{
 		$domain = self::getDomain();
-		return Config::$azure['$appTenantDomainName'][$domain];
+		return self::$appTenantDomainName[$domain];
 	}
 
 	public static function getApiVersion()
 	{
 		$domain = self::getDomain();
-		return Config::$azure['$apiVersion'][$domain];
+		return self::$apiVersion[$domain];
 	}
 }
