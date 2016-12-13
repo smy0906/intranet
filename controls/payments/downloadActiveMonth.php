503062461e4ca03642f45b2ac8a13c5342ae99d8
blu <blu@ridi.com>
2016-12-13 12:12:40 +0900

    결제요청 "귀속월 기준 조회" 긴급 추가

---
+++ controls/payments/downloadActiveMonth.php
@@ -0,0 +1,29 @@
+<?php
+/** @var $this Intra\Core\Control */
+
+use Intra\Model\PaymentModel;
+use Intra\Service\Payment\PaymentDto;
+use Intra\Service\Payment\PaymentDtoFactory;
+use Intra\Service\Payment\UserPaymentRequestFilter;
+use Intra\Service\Payment\UserPaymentStatService;
+use Intra\Service\User\UserPolicy;
+use Intra\Service\User\UserSession;
+use Symfony\Component\HttpFoundation\Response;
+
+if (!UserPolicy::isPaymentAdmin(UserSession::getSelfDto())) {
+	return new Response("권한이 없습니다", 403);
+}
+
+/**
+ * @var $payments PaymentDto[]
+ */
+
+$request = $this->getRequest();
+$month = $request->get('month');
+$month = UserPaymentRequestFilter::parseMonth($month);
+$month = date('Y/m/1', strtotime($month));
+
+$payment_service = new UserPaymentStatService();
+$user_payment_model = new PaymentModel();
+$payments = PaymentDtoFactory::importFromDatabaseDicts($user_payment_model->getAllPaymentsByActiveMonth($month));
+return $payment_service->getCsvRespose($payments);
