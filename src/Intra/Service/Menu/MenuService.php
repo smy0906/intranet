e0aa2233bcb0d38e814090790dcaf0079f508a08
blu <blu@ridi.com>
2017-01-06 14:50:09 +0900

    Update text in Support menu

--- src/Intra/Service/Menu/MenuService.php
+++ src/Intra/Service/Menu/MenuService.php
@@ -17,29 +17,38 @@ class MenuService
 	public static function addToSilexTwig(Application $app)
 	{
 		list($left_menu_list, $right_menu_list) = self::getMenuLinkList();
-		$app->extend('twig', function (Twig_Environment $twig, $app) use ($left_menu_list, $right_menu_list) {
-			$twig->addGlobal('leftMenuList', $left_menu_list);
-			$twig->addGlobal('rightMenuList', $right_menu_list);
-			return $twig;
-		});
+		$app->extend(
+			'twig',
+			function (Twig_Environment $twig, $app) use ($left_menu_list, $right_menu_list) {
+				$twig->addGlobal('leftMenuList', $left_menu_list);
+				$twig->addGlobal('rightMenuList', $right_menu_list);
+
+				return $twig;
+			}
+		);
 	}
 
 	/**
 	 * @return array
 	 */
-	public static function getMenuLinkList():array
+	public static function getMenuLinkList(): array
 	{
 		if (UserSession::isLogined()) {
 			if (Config::$domain == 'ridi.com') {
 				$left_menu_list = [
 					new Link('직원찾기', '/users', new PublicAuth),
-					new Link('리디 생활 가이드', 'https://ridicorp.sharepoint.com/intranet/SitePages/%EB%A6%AC%EB%94%94%20%EC%83%9D%ED%99%9C%20%EA%B0%80%EC%9D%B4%EB%93%9C.aspx', null, '_blank'),
+					new Link(
+						'리디 생활 가이드',
+						'https://ridicorp.sharepoint.com/intranet/SitePages/%EB%A6%AC%EB%94%94%20%EC%83%9D%ED%99%9C%20%EA%B0%80%EC%9D%B4%EB%93%9C.aspx',
+						null,
+						'_blank'
+					),
 					new Link('전사 주간 업무 요약', '/weekly', new ExceptTaAuth, '_blank'),
 					new Link('회의실', '/Rooms', new PublicAuth),
 					new Link('포커스룸', '/Rooms?type=focus'),
 					new Link('휴가신청', '/holidays', new PublicAuth),
 					'지원요청(테스트 중)' => [
-						new Link('기기 설치/장애 문의', '/Support/' . SupportPolicy::TYPE_DEVICE),
+						new Link('업무환경 불편사항 문의', '/Support/' . SupportPolicy::TYPE_DEVICE),
 						new Link('경조 지원', '/Support/' . SupportPolicy::TYPE_FAMILY_EVENT),
 						new Link('명함 신청', '/Support/' . SupportPolicy::TYPE_BUSINESS_CARD),
 						new Link('구매 요청', '/Support/' . SupportPolicy::TYPE_DEPOT),
@@ -71,10 +80,12 @@ class MenuService
 				new Link('내정보', '/users/myinfo', new PublicAuth, null, 'cog'),
 				new Link('로그아웃', '/usersession/logout', new PublicAuth, null, 'log-out'),
 			];
+
 			return [$left_menu_list, $right_menu_list];
 		} else {
 			$left_menu_list = [];
 			$right_menu_list = [];
+
 			return [$left_menu_list, $right_menu_list];
 		}
 	}
--- src/Intra/Service/Support/SupportPolicy.php
+++ src/Intra/Service/Support/SupportPolicy.php
@@ -96,7 +96,7 @@ class SupportPolicy
 	private static function initColumnFields()
 	{
 		self::$column_titles = [
-			self::TYPE_DEVICE => '기기 설치/장애 문의',
+			self::TYPE_DEVICE => '업무환경 불편사항 문의',
 			self::TYPE_FAMILY_EVENT => '경조 지원',
 			self::TYPE_BUSINESS_CARD => '명함 신청',
 			self::TYPE_DEPOT => '구매 요청',
@@ -116,7 +116,7 @@ class SupportPolicy
 				'인사팀 처리자' => new SupportColumnCompleteUser('completed_uid', 'is_completed'),
 				'인사팀 처리시각' => new SupportColumnCompleteDatetime('completed_datetime', 'is_completed'),
 				'귀속부서' => new SupportColumnTeam('team'),
-				'구분' => new SupportColumnCategory('category', ['전산 장애문의', 'SW 설치문의', '기타 장애문의']),
+				'구분' => new SupportColumnCategory('category', ['사내 전산 H/W 장애문의', 'SW 설치문의', '기타 장애문의']),
 				'상세내용' => new SupportColumnText('detail', '', '상세내용'),
 				'조치희망일' => new SupportColumnDate('request_date', date('Y/m/d'), true),
 				'비고' => new SupportColumnText('note', '', '비고'),
@@ -166,6 +166,7 @@ class SupportPolicy
 				'화환 연락처' => (new SupportColumnText('flower_call', '', '010-1234-5678'))->isRequired(),
 				'화환 주소' => (new SupportColumnText('flower_address'))->isRequired(),
 				'화환 도착일시' => (new SupportColumnDatetime('flower_datetime'))->placeholder('2016-01-02 07:10')->setTextInputType('datetime-local'),
+				'증빙서류' => new SupportColumnFile('paper'),
 				'비고' => new SupportColumnText('note', '', '비고'),
 			],
 			self::TYPE_BUSINESS_CARD => [
@@ -225,21 +226,17 @@ class SupportPolicy
 				'분류' => new SupportColumnCategory(
 					'category',
 					[
-						'일반구매 (사무용품, IT비품, 기타 소모품 등)',
-						'데스크탑 (개발/UX)',
-						'데스크탑 (디자이너)',
-						'노트북 (일반)',
-						'MAC (IMAC)',
-						'MAC (MACBOOK)',
-						'MAC (MAC MINI)',
-						'모니터 (24인치)',
-						'모니터 (27인치)',
-						'기기 (테블릿, 와콤 등)'
+						'일반구매 (사무용품, 전산/기타 소모품, 테스트기기 등)',
+						'모니터',
+						'MAC (맥북, 아이맥)',
+						'노트북',
+						'데스크탑',
+						'상품권 제작'
 					]
 				),
-				'상세내역' => new SupportColumnText('detail'),
-				'세팅(예정)일' => (new SupportColumnDate('request_date', date('Y/m/d', strtotime('+7 day')), true))->setTextInputType('date'),
-				'비고' => new SupportColumnText('note', '', '구매 사이트 링크 / 비고'),
+				'품목/수량' => new SupportColumnText('detail'),
+				'수령희망일' => (new SupportColumnDate('request_date', date('Y/m/d', strtotime('+7 day')), true))->setTextInputType('date'),
+				'URL 링크' => new SupportColumnText('note', '', '구매 사이트 링크 / 비고'),
 				'보유여부' => (new SupportColumnCategory('is_exist', ['재고', '신규구매']))->isVisibleIf($callback_is_human_manage_team),
 				'라벨번호' => (new SupportColumnText('label'))->isVisibleIf($callback_is_human_manage_team),
 			],
